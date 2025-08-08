a831ba70cc78a61ff1f1f975bfcadf33eeb7f985
UFC_UPDATES <ufcupdates313@gmail.com>
2025-08-08 16:52:36 +0530

    Update play.py

--- RishuMusic/utils/inline/play.py
+++ RishuMusic/utils/inline/play.py
@@ -64,7 +64,7 @@ def stream_markup_timer(_, chat_id, played, dur):
          InlineKeyboardButton(text=_["S_B_3"], url=f"https://t.me/{app.username}?startgroup=true",)
         ],
         [
-         InlineKeyboardButton(text="• ᴏᴡɴᴇʀ •", user_id=config.OWNER_ID),
+         InlineKeyboardButton(text="• 𝐇єᴧʀ፝֠֩ᴛʙєᴧᴛ •", user_id=config.OWNER_ID),
          InlineKeyboardButton(text="• ɢʀᴏᴜᴘ •", url=f"{SUPPORT_CHAT}",),
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
@@ -75,7 +75,7 @@ def stream_markup_timer(_, chat_id, played, dur):
 def stream_markup(_, chat_id):
     buttons = [
         [
-         InlineKeyboardButton(text="• ᴏᴡɴᴇʀ •", user_id=config.OWNER_ID),
+         InlineKeyboardButton(text="• 𝐇єᴧʀ፝֠֩ᴛʙєᴧᴛ •", user_id=config.OWNER_ID),
          InlineKeyboardButton(text="• ɢʀᴏᴜᴘ •", url=f"{SUPPORT_CHAT}",),
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
